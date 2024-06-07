1ac6f0dbd0a2a585ccc9cf0394bdf2f97686587a
Benoit <benoit.exbrayat@gmail.com>
2024-06-07 16:13:19 +0200

    add the logic of creating projects and store them
    add the logic in projects.js to initialize the
        structure of projects and tasks
    add the logic of localstorage

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,22 @@
-const initializeProjects = () => {
-    const DefaultTask = new Task ("Main", "First Task", "This is the default task initialized", dueDate, "High", ["item1", "item2", "item3"], [false, true, false]); 
-    
+import { format} from "date-fns";
+
+class Project {
+    constructor(name, projectTasks){
+        this.name = name; 
+        this.projectTasks = projectTasks; 
+    }
 }
+
 class Task {
-    constructor() {
+    constructor(
+        projectName, 
+        taskName, 
+        taskDescription, 
+        dueDate,
+        priorityRate, 
+        checkListItensNames, 
+        checkListItensStatus
+        ) {
         this.projectName = projectName; 
         this.taskName = taskName; 
         this.taskDescription = taskDescription; 
@@ -13,5 +26,33 @@ class Task {
         this.checkListItensStatus = checkListItensStatus; 
     } 
 }
+
+const Projects = []; 
+const DefaultProject = new Project("Main", []);
+const DefaultTask = new Task ("Main", "First Task", "This is the default task initialized", format(new Date(2014, 1, 11), "dd/MM/yyyy"), "High", ["item1", "item2", "item3"], [false, true, false]); 
+    
+const initializeProjects = () => { 
+    addTaskToProject(DefaultTask, DefaultProject); 
+    saveProject(DefaultProject); 
+    storeProjects();  
+    console.log(retrieveStoredProjects() == Projects); 
+}
+
+function retrieveStoredProjects(){
+    return localStorage.getItem("StoredProjects");
+}
+
+function storeProjects() {
+    localStorage.setItem("StoredProjects", Projects);
+}
+
+function addTaskToProject(task, project){
+    project.projectTasks.push(task); 
+}
+
+function saveProject(project){
+    Projects.push(project); 
+}
+
 
 export default initializeProjects; 
